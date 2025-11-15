31dd3d4ddb66f43238e9bf37f8f91cb4aba42586
Furkan Albayrak <furkanalb42@gmail.com>
2025-11-15 18:14:14 +0300

    weather-app projesi eklendi

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,58 @@
+
+import { use, useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+
+  const [cityName, setCityName] = useState("");
+  const [data, setData] = useState();
+
+  const getDataFromAPI = async () => {
+
+    if (cityName) {
+
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ac5a7d5afecb462a2d238efacc8cd6b6&units=metric&lang=tr`);
+        setData(response.data);
+      } catch (error) {
+        alert("Girdiginiz şehir bulunamadı!!!");
+      }
+
+    }
+    else {
+      alert("Bir Şehir giriniz");
+    }
+  }
+
+  return (
+    <div>
+      <div className='container'>
+
+        <div className='firstContainer'>
+          <h1>Hava Durumu</h1>
+          <input className='getCity' type="text" placeholder='Aramak istediginiz şehri giriniz' value={cityName} onChange={(e) => setCityName(e.target.value)} />
+          <button className='search' onClick={getDataFromAPI} >Ara</button>
+        </div>
+
+        {
+          data ? (
+            <div className='secondContainer'>
+              <h2 className='isim' >{data.name}</h2>
+              <p className='child'>Sıcaklık: {data.main.temp} °C </p>
+              <p className='child'>Hissedilen Sıcaklık: {data.main.feels_like} °C</p>
+              <p className='child'>Hava Durumu: {data.weather[0].description}</p>
+              <p className='child'>Nem Oranı: {data.main.humidity}%</p>
+              <p className='child'>Ruzgar Hızı: {data.wind.speed} km/s</p>
+            </div>
+
+          )
+            : null
+        }
+
+      </div>
+    </div>
+  )
+}
+
+export default App
